161e600f181fc136f3fdb4f2b6adcc303c7af154
sangki810 <113147507+sangki810@users.noreply.github.com>
2022-10-16 04:11:23 -0400

    worked on js functions, matched html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
 // Global variables
 var myAPIKey = '79ccccbf056ad9d002777e1e5b09a098';
 var city;
-var searchHistoryArr = [];
+var searchHistoryArr = "";
 var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${myAPIKey}`;
-
+var date = dayjs().format(MM/DD/YYY);
 // DOM element references
 var cityInputEl = document.getElementById('city-input');
 var todaysWeatherEl = document.getElementById('todays-weather');
 var forecastEl = document.getElementById('weekly-forecast');
 var searchHistoryEl = document.getElementById('search-history');
 var searchBtnEl = document.getElementById('search-btn')
-
+var cityNameDateEl = document.getElementById('city-name-date')
 var tempEl =  document.getElementById('temperature');
 var windEl = document.getElementById('wind');
 var humidityEl = document.getElementById('humidity');
@@ -25,6 +25,7 @@ function renderSearchHistory() {
     var cityBtn = document.createElement("button");
     cityBtn.textContent = city
     // append to the search history container
+    cityBtn.setAttribute("class", "form-control mt-3")
     searchHistoryEl.append(cityBtn)
   }
 }
@@ -32,7 +33,7 @@ function renderSearchHistory() {
 // Function to update history in local storage then updates displayed history.
 function appendToHistory(search) {
   // push search term into search history array
-  searchHistoryArr.push(search)
+  //searchHistoryArr.push(searchHistoryEl.value)
   // set search history array to local storage
   localStorage.setItem("history", JSON.stringify(searchHistoryArr));
   renderSearchHistory();
@@ -41,32 +42,28 @@ function appendToHistory(search) {
 // Function to get search history from local storage
 function initSearchHistory() {
   // get search history item from local storage
-  if (localStorage.getItem("history") !== null) { 
-    // set search history array equal to what you got from local storage
-    searchHistoryArr = JSON.parse(localStorage.getItem("history"))
-  } else {
-    localStorage.setItem("history", JSON.stringify(searchHistoryEl))
-  }
-   
-    
+  localStorage.getItem("history") 
+  // set search history array equal to what you got from local storage
+  searchHistoryArr = (localStorage.getItem("history"))
   renderSearchHistory();
 }
   
 // Function to display the CURRENT weather data fetched from OpenWeather api.
 function renderCurrentWeather(city, weather) {
+  todaysWeatherEl.innerHTML = "";
   todaysWeatherEl.classList.remove('d-none')
   // Store response data from our fetch request in variables
   // temperature, wind speed, etc.
-  var temp = weather.current.temp;
-  var wind = weather.current.wind_speed;
-  var humidity = weather.current.humidity;
-  var clouds = weather.current.clouds;
+  var temp = weather.main.temp;
+  var wind = weather.wind.speed;
+  var humidity = weather.main.humidity;
+  var clouds = weather.weather[0].main;
   // document.create the elements you'll want to put this information in  
   
   // append those elements somewhere
     
   // give them their appropriate content
-  cityDateEl.textContent = [city + " " + currentDay]
+  cityNameDateEl.textContent = [city + " " + date]
   tempEl.textContent = ["Temp: " + temp + "°C"]
   windEl.textContent = ["Wind: " + wind + "kph"]
   humidityEl.textContent = ["Humidity: " + humidity + "%"]
@@ -76,6 +73,7 @@ function renderCurrentWeather(city, weather) {
 // Function to display a FORECAST card given an object (from our renderForecast function) from open weather api
 // daily forecast.
 function renderForecastCard(forecast) {
+  forecastEl.innerHTML = "";
   forecastEl.classList.remove('d-none')
   // variables for data from api
     // temp, windspeed, etc.
@@ -117,46 +115,46 @@ function fetchWeather(lat, lon, city) {
   var cityLon = lon;
   var cityName = city;
   // api url
-  var apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${cityLat}&lon=${cityLon}&units=imperial&exclude=minutely,hourly,alerts&appid=${myAPIKey}`
+  var weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${cityLat}&lon=${cityLon}&units=imperial&exclude=minutely,hourly,alerts&appid=${myAPIKey}`
   // fetch, using the api url, .then that returns the response as json, .then that calls renderItems(city, data)
-  fetch(apiUrl)
+  fetch(weatherUrl)
   .then(function (response) {
     if (response.ok) {
-      response.json().then(function (data) {
+      response.json()
+      .then(function (data) {
         renderItems(cityName, data);
       })
+    } else {
+      alert('Error: could not retrieve data');
     }
-  })
+  });
 }
   
 function fetchCoords(search) {
   // variable for you api url
   var city = search;
   // fetch with your url, .then that returns the response in json, .then that does 2 things - calls appendToHistory(search), calls fetchWeather(the data)
-  var geoURL = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${myAPIKey}`;
+  var geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${myAPIKey}`;
 
-  fetch(geoURL)
+  fetch(geoUrl)
   .then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
         var lat = data[0].lat;
-        var lon = data[0],lon;
+        var lon = data[0].lon;
         appendToHistory(search)
         fetchWeather(lat, lon, city);
       });
     } else {
-      alert('Error: ' + response.statusText);
+      alert('Error: could not retrieve data');
     }
-  })
-  .catch (function (error) {
-    alert("Cannot connect to Openweathermap API");
   });
 }
   
 function handleSearchFormSubmit(e) {
   // Don't continue if there is nothing in the search form
   if (!cityInputEl.value) {
-    alert("Enter a city name display weather")
+    alert("Enter a city name to display weather")
     return;
   }
   
@@ -178,4 +176,4 @@ initSearchHistory();
 // click event to run the handleFormSubmit
 searchBtnEl.addEventListener("click", handleSearchFormSubmit);
 // click event to run the handleSearchHistoryClick
-searchHistoryEl,addEventListener('click', handleSearchHistoryClick);
+searchHistoryEl.addEventListener('click', handleSearchHistoryClick);
